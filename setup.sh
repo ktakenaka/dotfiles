0f200ed3d88af469369048ea9b4eecc50f790ce7
takenaka-kazumasa <takenaka.kazumasa@moneyforward.co.jp>
2019-07-15 09:14:41 +0700

    organize setting files

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
 #!/usr/local/bin/zsh
 
-DOT_FILES=(.zshrc .zprofile .zshenv .inputrc .vimrc .gvimrc .gitignore_global)
-
-for file in ${DOT_FILES[@]}
+echo 'Create symlinks'
+DIRS=(iterm_files terminal_files vim_files zsh_files)
+DOT_HOME=$HOME/dotfiles
+for dir in ${DIRS[@]}
 do
-   ln -s $HOME/dotfiles/$file $HOME/$file
+  for f in $(ls -A $DOT_HOME/$dir)
+  do
+    ln -fs "$DOT_HOME/$dir/$f" $HOME/$f
+  done
 done
 
+echo 'Setting for atom'
 rm -f $HOME/.atom/keymap.cson
 ln keymap.cson $HOME/.atom/keymap.cson
 
+echo 'Setting for python'
 rm -f $HOME/.ipython/profile_default/startup/default.py
 ln -s $HOME/dotfiles/default.py $HOME/.ipython/profile_default/startup/default.py
-jt -r
