03b73e80cf1a17fdf466eb49bb5531eb19408073
Takenaka Kazumasa <bzzs.1120@gmail.com>
2020-10-06 01:16:19 +0700

    refactor ubuntu setting

--- ubuntu/.zprofile
+++ ubuntu/.zprofile
@@ -2,8 +2,3 @@
 # It will raise `Can't connect to display ":0"` error without this command.
 xhost + > /dev/null
 alias kymap="sudo /etc/init.d/ktake_xkeymap start"
-
-if [ -x "`which go`" ]; then
-  export GOPATH=$HOME/go
-  export PATH="$GOPATH/bin:$PATH"
-fi
---
+++ ubuntu/setup.sh
@@ -0,0 +1,17 @@
+DOT_HOME=$HOME/dotfiles
+UBUNTU_HOME=$HOME/dotfiles/ubuntu
+
+# --- zsh ---
+rsync $UBUNTU_HOME/.zprofile $HOME/
+rsync $DOT_HOME/zsh/ $HOME/
+
+# --- vim ---
+rsync -av $DOT_HOME/vim/ $HOME/
+
+# init.d template came from here https://github.com/fhd/init-script-template
+sudo rsync $UBUNTU_HOME/xkeysnail/ktake_xkeysnail /etc/init.d/
+sudo chown root /etc/init.d/ktake_xkeysnail
+sudo chmod a+x /etc/init.d/ktake_xkeysnail
+
+sudo mkdir -p /etc/xkeysnail
+sudo rsync $UBUNTU_HOME/xkeysnail/config.py /etc/xkeysnail
